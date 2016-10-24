4cb76a498c9883db4c618897d654cd60c4ec321e
System Administrator <root@wireless-10-146-126-163.public.utexas.edu>
2016-10-23 19:18:51 -0500

    pit 1.1.11 works

--- pit-example/pit.sh
+++ pit-example/pit.sh
@@ -4,7 +4,7 @@ JAVA_AGENT="/Users/ahmad/aspectj1.8/lib/aspectjweaver.jar"
 ASPECTS_JAR="aspects.jar"
 MAIN_CLASS=""
 
-java -javaagent:${JAVA_AGENT}  -cp ${ASPECTS_JAR}:target/classes:target/test-classes:lib/junit-4.10.jar:lib/pitest-1.1.10.jar:lib/pitest-command-line-1.1.10.jar  \
+java -javaagent:${JAVA_AGENT}  -cp ${ASPECTS_JAR}:target/classes:target/test-classes:lib/junit-4.10.jar:lib/pitest-1.1.11-SNAPSHOT.jar:lib/pitest-command-line-1.1.10.jar  \
     org.pitest.mutationtest.commandline.MutationCoverageReport \
     --reportDir target/pit-reports \
     --targetClasses pitexample.* \
