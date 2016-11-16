f4d5a6cc4dbe0878ec3a856e1e1615764ac4c5a8
System Administrator <root@wireless-10-146-177-238.public.utexas.edu>
2016-11-15 19:43:43 -0600

    progress

--- pit-example/pitest_source/pitest/src/main/java/org/pitest/mutationtest/tooling/MutationSelectEngine.java
+++ pit-example/pitest_source/pitest/src/main/java/org/pitest/mutationtest/tooling/MutationSelectEngine.java
@@ -1,10 +1,11 @@
 package org.pitest.mutationtest.tooling;
 
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 import org.pitest.mutationtest.MutationMetaData;
 import org.pitest.mutationtest.MutationResult;
@@ -17,6 +18,10 @@ public class MutationSelectEngine {
 	private List<MutationAnalysisUnit> allMAU; //obtained from first iteration outside the loop TUS
 	private List<MutationAnalysisUnit> mau_per_categ; // My first Sample
 	
+	private Map<String,Integer> categ_prior;
+	
+	List<List<MutationDetails>> mutations_available;
+	
 	//private List<MutationAnalysisUnit> mutants_killed;
 	//private List<MutationAnalysisUnit> mutants_alive;
 
@@ -24,8 +29,16 @@ public class MutationSelectEngine {
 	public MutationSelectEngine(List<MutationAnalysisUnit> tus){
 		allMAU = new ArrayList<MutationAnalysisUnit>(tus);
 		mau_per_categ = new ArrayList<MutationAnalysisUnit>();
-	}	
-	
+		categ_prior = new HashMap<String,Integer>();
+		mutations_available = new ArrayList<List<MutationDetails>>( tus.size() );
+		
+		// SAVE MUTATIONS AVAILABLE
+		
+		for(int i = 0; i < tus.size(); ++i ){		
+			for(MutationDetails md : ((MutationTestUnit)tus.get(i)).getMutations())
+				mutations_available.get( i ).add(md);
+		}	
+	}
 	public ArrayList<MutationResult> get_MR(MutationAnalysisUnit MAU){
 		ArrayList<MutationResult> MR = new ArrayList<MutationResult>( ((MutationTestUnit) MAU).AllMutationState.getMutations());
 		return MR;
@@ -69,28 +82,68 @@ public class MutationSelectEngine {
 	}
 
 		
-	public void constructAlive( List<MutationMetaData> runResult ) {
+	// return alive categories
+	public List<String> constructAlive( List<MutationMetaData> runResult ){
+		List<String> categ = new ArrayList<String>();
 		for(MutationMetaData mmd : runResult) {
 			for(MutationResult mr : mmd.getMutations()) {
-				if(!mr.getStatusDescription().equals("?")) {
-					
+				//Alive
+				if(!mr.getStatusDescription().equals("KILLED")) {
+					categ.add(mr.getDetails().getMutator());
 				}
 			}
 		}
+		return categ;
 	}
 	
-	public void update(MutationMetaData MMD) { }
+	// Update priority
+	public void update(List<MutationMetaData> runResult){
+		List<String> categories = constructAlive(runResult);
+		
+		for(int i = 0; i < categories.size(); i++){
+			String key = categories.get(i);
+			Integer value = categ_prior.get(key);
+			value++;	
+			categ_prior.put(key,value);
+		}
+		
+	}
+	
+	
+	// choose categories: certain percentage
+	// each category: choose an alive one
+	// update mau_per_categ.setmutations	
 	
 	// use: update prior_categ to read priority and choose randomly
 	// for example: 70% from highest prioriy and 30% for lowest
 	// return tus
 	// mutants_alive is argument for run in mutation
-	public List<MutationAnalysisUnit> selectMutants() {
+	
+	public List<MutationAnalysisUnit> selectMutants(List<MutationMetaData> runResult) {
+		update(runResult);
+		List<String> favorite_categ = new ArrayList<String>();
+
+		// NEED TO SORT FIRST
+	    int count = 0;
+		// choose categories: certain percentage
+ 		for (String key: categ_prior.keySet()) {
+ 			if(count % 2 == 0)
+ 				favorite_categ.add(key);
+ 			count++;
+ 		}
+		
+ 		// each category: choose an alive one
+
+ 		for(MutationAnalysisUnit mau : mau_per_categ){
+ 			
+ 			
+ 		}
+ 		
 		List<MutationAnalysisUnit> filteredList = new ArrayList<MutationAnalysisUnit>();
 		for( MutationAnalysisUnit mau : allMAU ) {
 			filteredList.add(mau);
 		}
 		return filteredList;
 	}	
-	
+
 }
