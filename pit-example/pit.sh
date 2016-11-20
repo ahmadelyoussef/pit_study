5394422995bde1a8f40de812c1a433a6d06863f5
System Administrator <root@wireless-10-146-93-195.public.utexas.edu>
2016-11-19 21:51:55 -0600

    working with test suite

--- pit-example/pit.sh
+++ pit-example/pit.sh
@@ -1,6 +1,6 @@
 java -cp target/classes:target/test-classes:lib/junit-4.10.jar:lib/pitest-1.1.11-SNAPSHOT.jar:lib/pitest-command-line-1.1.11-SNAPSHOT.jar \
     org.pitest.mutationtest.commandline.MutationCoverageReport \
     --reportDir target/pit-reports \
-    --targetClasses com.* \
+    --targetClasses com.squareup.javapoet.* \
+    --targetTests com.squareup.javapoet.* \
     --sourceDirs src/main/java,src/test/java
-
