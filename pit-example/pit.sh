f8df06b8a34350a44be5eef25384d7acb44bf292
System Administrator <root@wireless-10-146-93-195.public.utexas.edu>
2016-11-19 19:39:22 -0600

    test

--- pit-example/pit.sh
+++ pit-example/pit.sh
@@ -1,6 +1,6 @@
 java -cp target/classes:target/test-classes:lib/junit-4.10.jar:lib/pitest-1.1.11-SNAPSHOT.jar:lib/pitest-command-line-1.1.11-SNAPSHOT.jar \
     org.pitest.mutationtest.commandline.MutationCoverageReport \
     --reportDir target/pit-reports \
-    --targetClasses pitexample.* \
+    --targetClasses com.* \
     --sourceDirs src/main/java,src/test/java
 
